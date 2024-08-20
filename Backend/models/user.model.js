2c65fbf0f5c56ff68d6190a2932c9e569b500664
abhishekumar09 <2003abhishek.09@gmail.com>
2024-08-20 23:04:42 +0530

    backend done

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -5,7 +5,7 @@ const userSchema = mongoose.Schema({
         type: String,
         require: true,
     },
-    email:{
+    email:{  
         type: String,
         require: true,
         lowercase: true,
